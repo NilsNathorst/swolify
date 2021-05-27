8285453fb1b58c6977f73e4677faa7bb8b4029dc
Nils Nathorst-Windahl <nils.nathorst-windahl@universalavenue.com>
2021-05-27 09:24:41 +0200

    increase gains

--- src/components/Excercise/Excercise.jsx
+++ src/components/Excercise/Excercise.jsx
@@ -1,7 +1,9 @@
-export const Excercise = ({ name, sets, repetitions, weight }) => {
+export const Excercise = ({ name, sets, repetitions, weight, type }) => {
     return (
         <div>
-            <h3>{name}</h3>
+            <h3>
+                {name} {type.icon}
+            </h3>
             <span>
                 {`${sets} x ${repetitions}`} {weight && ` / ${weight} kg`}
             </span>
